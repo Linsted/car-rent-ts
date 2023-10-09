f6b3c7042fd042601971ead36973c6e25006926b
Linsted <ivan.malenko18@gmail.com>
2023-10-09 19:12:23 +0300

    fix: change overlay position

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -29,8 +29,8 @@ export default function Hero() {
             fill
             className="object-contain"
           />
-          <div className="hero__image-overlay" />
         </div>
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
