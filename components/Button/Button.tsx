cb3b86eb1ee97c7e5b04eb844c9ab30cb77f0cbb
Linsted <ivan.malenko18@gmail.com>
2023-10-22 14:14:54 +0300

    fix: update Button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,25 +1,40 @@
 "use client";
 
 import React from "react";
+import Image from "next/image";
 
 import { ButtonProps } from "@/types";
-
-import { BUTTONS_TYPES } from "./constants";
+import { BUTTONS_TYPES } from "@/helpers/global/constants/constants";
 
 export default function Button({
   title,
-  containerStyles,
-  handleClick,
+  containerStyles = "",
+  handleClick = () => {},
   type = BUTTONS_TYPES.BUTTON,
+  textStyles = "",
+  buttonIcon,
+  isDisabled = false,
+  iconWidth,
+  iconHeight,
+  iconAlt = "icon",
 }: ButtonProps) {
   return (
     <button
-      disabled={false}
+      disabled={isDisabled}
       type={type}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`${textStyles} flex-1`}>{title}</span>
+      {buttonIcon && (
+        <Image
+          className="object-contain"
+          src={buttonIcon}
+          width={iconWidth}
+          height={iconHeight}
+          alt={iconAlt}
+        />
+      )}
     </button>
   );
 }
