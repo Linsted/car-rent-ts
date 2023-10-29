5c56c8fe5ba044305539ca289ca4dfe20b7c6cc3
Linsted <ivan.malenko18@gmail.com>
2023-10-29 12:49:36 +0200

    feat: add SearchModel component

---
+++ components/SearchModel/SearchModel.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "next/image";
+
+import { PATHS } from "@/helpers/global/constants/constants";
+import { SearchModelProps } from "@/types";
+
+import {
+  MODEL_ICON_DIMENSIONS,
+  MODEL_IMAGE_ALT,
+  INPUT_ATTRIBUTES,
+} from "./constants";
+
+export default function SearchModel({ model, handleInput }: SearchModelProps) {
+  return (
+    <>
+      <Image
+        src={PATHS.MODEL_ICON}
+        width={MODEL_ICON_DIMENSIONS.WIDTH}
+        height={MODEL_ICON_DIMENSIONS.HEIGHT}
+        className="absolute w-[20px] h-[20px] ml-4 bottom-22"
+        alt={MODEL_IMAGE_ALT}
+      />
+      <input
+        type={INPUT_ATTRIBUTES.TYPE}
+        name={INPUT_ATTRIBUTES.NAME}
+        value={model}
+        onChange={handleInput}
+        placeholder={INPUT_ATTRIBUTES.PLACEHOLDER}
+        className="searchbar__input"
+      />
+    </>
+  );
+}
---
+++ components/SearchModel/constants.ts
@@ -0,0 +1,12 @@
+export const MODEL_ICON_DIMENSIONS = Object.freeze({
+  WIDTH: 25,
+  HEIGHT: 25,
+});
+
+export const MODEL_IMAGE_ALT = "car model";
+
+export const INPUT_ATTRIBUTES = Object.freeze({
+  TYPE: "text",
+  NAME: "model",
+  PLACEHOLDER: "Golf",
+});
